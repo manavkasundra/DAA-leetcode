5dd13f7fd38921a03fa854ba9238f5502f6443f7
manavkasundra <manavkasundra.127453@marwadiuniversity.ac.in>
2026-08-03 21:16:42 +0530

    Time: 15 ms (63.89%), Space: 56.9 MB (90.04%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    ListNode* findMid(ListNode* head) {
+        ListNode *slow = head, *fast = head->next;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+
+    ListNode* merge(ListNode* a, ListNode* b) {
+        ListNode dummy(0);
+        ListNode* cur = &dummy;
+
+        while (a && b) {
+            if (a->val < b->val) {
+                cur->next = a;
+                a = a->next;
+            } else {
+                cur->next = b;
+                b = b->next;
+            }
+            cur = cur->next;
+        }
+
+        cur->next = a ? a : b;
+        return dummy.next;
+    }
+
+    ListNode* sortList(ListNode* head) {
+        if (!head || !head->next) return head;
+
+        ListNode* mid = findMid(head);
+        ListNode* right = mid->next;
+        mid->next = NULL;
+
+        ListNode* left = sortList(head);
+        right = sortList(right);
+
+        return merge(left, right);
+    }
+};
