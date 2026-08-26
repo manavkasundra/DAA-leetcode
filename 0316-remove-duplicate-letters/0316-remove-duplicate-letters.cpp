8169feb7ba5553d80b202c50fcd341ed047a44a9
manavkasundra <manavkasundra.127453@marwadiuniversity.ac.in>
2026-08-26 23:24:12 +0530

    Time: 0 ms (100%), Space: 9.3 MB (62.85%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        vector<int> lastIndex(26, 0);
+        for (int i = 0; i < s.length(); i++) {
+            lastIndex[s[i] - 'a'] = i; 
+        }
+        
+        vector<bool> seen(26, false);
+        string result = ""; 
+        
+        for (int i = 0; i < s.length(); i++) {
+            char curr = s[i];
+            
+            if (seen[curr - 'a']) {
+                continue;
+            }
+            
+            while (!result.empty() && result.back() > curr && lastIndex[result.back() - 'a'] > i) {
+                seen[result.back() - 'a'] = false; 
+                result.pop_back(); 
+            }
+            
+            result.push_back(curr);
+            seen[curr - 'a'] = true;
+        }
+        
+        return result;
+    }
+};
